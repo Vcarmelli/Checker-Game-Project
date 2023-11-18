4cc8a50334fd6088732ffc9fd1edab523d8b634a
Vcarmelli <vashti.karmelli16@gmail.com>
2023-11-18 20:20:31 +0800

    organized files

---
+++ components/clock.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var startTime = new Date();
+
+    function updateClock() {
+        var currentTime = new Date();
+        var elapsedTime = new Date(currentTime - startTime);
+
+        var hours = elapsedTime.getUTCHours().toString().padStart(2, '0');
+        var minutes = elapsedTime.getUTCMinutes().toString().padStart(2, '0');
+        var seconds = elapsedTime.getUTCSeconds().toString().padStart(2, '0');
+
+        document.getElementById('timeElapsed').innerText = hours + ':' + minutes + ':' + seconds;
+    }
+
+    setInterval(updateClock, 1000);
+
+    updateClock();
+});
